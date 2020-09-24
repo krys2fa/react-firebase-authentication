5b605bfd73d25bdb3d4c1eb9d3df03ef5a369a47
Christopher Amanor <kriskit7@gmail.com>
2020-09-24 23:08:35 +0000

    Set up firebase configuration

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDR44rd_ZkrO-zQQY-QsCx28deRrqPjWjw',
+  authDomain: 'react-firebase-authentic-43220.firebaseapp.com',
+  databaseURL: 'https://react-firebase-authentic-43220.firebaseio.com',
+  projectId: 'react-firebase-authentic-43220',
+  storageBucket: 'react-firebase-authentic-43220.appspot.com',
+  messagingSenderId: '159943670953',
+  appId: '1:159943670953:web:c7ee8078ea398968db7db8',
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
