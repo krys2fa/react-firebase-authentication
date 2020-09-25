9f0f8ae28428a3e7f6ebd3ad3d48577f202cf6a6
Christopher Amanor <kriskit7@gmail.com>
2020-09-25 11:18:00 +0000

    Use React Context to pass authenticated user from higher order components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -10,57 +11,24 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
-import { withFirebase } from '../Firebase';
-// const App = () => (
-//   <Router>
-//     <Navigation />
+import { withAuthentication } from '../Session';
 
-//     <hr />
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
 
-//     <Route exact path={ROUTES.LANDING} component={LandingPage} />
-//     <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-//     <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-//     <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-//     <Route path={ROUTES.HOME} component={HomePage} />
-//     <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-//     <Route path={ROUTES.ADMIN} component={AdminPage} />
+      <hr />
 
-//   </Router>
-// );
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authUser: null,
-    };
-  }
-
-  componentDidMount() {
-    this.props.firebase.auth.onAuthStateChanged((authUser) => {
-      authUser
-        ? this.setState({ authUser })
-        : this.setState({ authUser: null });
-    });
-  }
-  
-  componentWillUnmount() {
-    this.listener();
-  }
-
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navigation authUser={this.state.authUser} />
-          <hr />
-          ...
-        </div>
-      </Router>
-    );
-  }
-}
-
-
-export default withFirebase(App);
+export default withAuthentication(App);
