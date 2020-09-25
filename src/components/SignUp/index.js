42763d1edf80a10b598eeba2a8f97e6595cfaefd
Christopher Amanor <kriskit7@gmail.com>
2020-09-25 09:40:02 +0000

    Implement SignOut button

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -29,7 +29,7 @@ class SignUpFormBase extends Component {
   }
 
   onSubmit = (event) => {
-    const { username, email, passwordOne } = this.state;
+    const { email, passwordOne } = this.state;
 
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
@@ -39,6 +39,8 @@ class SignUpFormBase extends Component {
       })
       .catch((error) => {
         this.setState({ error });
+        console.log(error);
+        alert(error);
       });
 
     event.preventDefault();
